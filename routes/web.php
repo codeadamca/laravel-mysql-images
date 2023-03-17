9b16d7e7c1c84b502d361cfcf965c7d691c34dfb
Adam Thomas <thomasadam83@hotmail.com>
2023-03-17 15:32:43 -0400

    Added seeding

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Link;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('links', [
+        'links' => Link::all(),
+    ]);
 });
